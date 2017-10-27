4367f2d0e2848b204a53125087e4dbd2bda84f1c
kunle-lawal <olawal196@gmail.com>
2017-10-27 14:14:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ $(document).ready(function(){
 		$("#navigationBar").toggleClass("closeNavigationBar");
 		$("#navigationBar").toggleClass("openNavigationBar");
 	})
+
+	$("#map").click(function(){
+		$(".mapContainer").toggleClass("hiddenMap");
+		$("#mapInfo").toggleClass("hidden");
+	}); 
+
+	//$('#navigationItems').localScroll({duration:800});
+
+
+	$('a').click(function(){
+    	$('html, body').animate({
+    	    scrollTop: $( $(this).attr('href') ).offset().top
+    	}, 500);
+    	return false;
+	});
 })
